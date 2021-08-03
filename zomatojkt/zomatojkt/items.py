338431bdef7c1d42d538361336f20c65a55e7fa7
M Muchsin <87637609+mmuchsin@users.noreply.github.com>
2021-08-03 18:59:30 +0700

    Update items.py
    add itemloader

--- zomatojkt/zomatojkt/items.py
+++ zomatojkt/zomatojkt/items.py
@@ -4,11 +4,14 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
 
 
 class ZomatojktItem(scrapy.Item):
     # define the fields for your item here like:
-    name = scrapy.Field()
+    name = scrapy.Field(input_proccessor =  MapCompose(remove_tags), output_proccessor = TakeFirst())
     url = scrapy.Field()
     address = scrapy.Field()
     rate = scrapy.Field()
