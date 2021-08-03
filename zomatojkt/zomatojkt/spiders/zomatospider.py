eace5c6cba177f1c0d9ce4fff682e197f4d1ffaa
M Muchsin <m.muchsin@yahoo.com>
2021-08-03 14:29:09 +0700

    Update zomatospider.py

--- zomatojkt/zomatojkt/spiders/zomatospider.py
+++ zomatojkt/zomatojkt/spiders/zomatospider.py
@@ -4,8 +4,14 @@ from zomatojkt.items import ZomatojktItem
 
 class ZomatospiderSpider(scrapy.Spider):
     name = 'zomatospider'
-    allowed_domains = ['www.zomato.com/jakarta/best-restaurants?page=1']
+    allowed_domains = ['www.zomato.com/']
     start_urls = ['http://https://www.zomato.com/jakarta/best-restaurants?page=']
+    
+    def start_requests(self, response):
+        max_page = response.css('#search-results-container > div.search-pagination-top.clearfix.mtop > div.row > div.col-l-4.mtop.pagination-number > div > b:nth-child(2)::text').get()
+        for i in range(1, int(max_page)+1):
+            yield scrapy.Request(f'https://www.zomato.com/jakarta/best-restaurants?page={i}', callback=self.parse)
 
     def parse(self, response):
-        pass
+        data = ZomatojktItem()
+        
