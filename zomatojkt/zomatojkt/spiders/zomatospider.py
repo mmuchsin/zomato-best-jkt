b27326328b2f8e1133a919a8093e08949f8a261b
M Muchsin <m.muchsin@yahoo.com>
2021-08-03 14:54:19 +0700

    Update zomatospider.py

--- zomatojkt/zomatojkt/spiders/zomatospider.py
+++ zomatojkt/zomatojkt/spiders/zomatospider.py
@@ -5,13 +5,20 @@ from zomatojkt.items import ZomatojktItem
 class ZomatospiderSpider(scrapy.Spider):
     name = 'zomatospider'
     allowed_domains = ['www.zomato.com/']
-    start_urls = ['http://https://www.zomato.com/jakarta/best-restaurants?page=']
+    start_urls = ['https://www.zomato.com/jakarta/best-restaurants?page=']
     
-    def start_requests(self, response):
-        max_page = response.css('#search-results-container > div.search-pagination-top.clearfix.mtop > div.row > div.col-l-4.mtop.pagination-number > div > b:nth-child(2)::text').get()
-        for i in range(1, int(max_page)+1):
-            yield scrapy.Request(f'https://www.zomato.com/jakarta/best-restaurants?page={i}', callback=self.parse)
+    # def start_requests(self, response):
+    #     max_page = response.css('#search-results-container > div.search-pagination-top.clearfix.mtop > div.row > div.col-l-4.mtop.pagination-number > div > b:nth-child(2)::text').get()
+    #     for i in range(1, int(max_page)+1):
+    #         yield scrapy.Request(f'https://www.zomato.com/jakarta/best-restaurants?page={i}', callback=self.parse)
 
     def parse(self, response):
         data = ZomatojktItem()
+        list_restaurant = response.css('div.card.search-snippet-card.search-card')
         
+        for i in range(len(list_restaurant)):
+            name = response.css('a.result-title.hover_feedback.zred.bold.ln24.fontsize0::text')
+            
+            data['name'] = name[i].get()
+            
+            yield data
