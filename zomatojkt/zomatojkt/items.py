b349be4b1d3be89b5111bdb9f55508b0a3680c94
M Muchsin <87637609+mmuchsin@users.noreply.github.com>
2021-08-04 09:26:42 +0700

    itemloader implementation

--- zomatojkt/zomatojkt/items.py
+++ zomatojkt/zomatojkt/items.py
@@ -4,19 +4,21 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy.loader import ItemLoader
-from itemloaders.processors import TakeFirst, MapCompose
+from itemloaders.processors import TakeFirst, MapCompose, Join
 from w3lib.html import remove_tags
-
 
 class ZomatojktItem(scrapy.Item):
     # define the fields for your item here like:
-    name = scrapy.Field(input_proccessor =  MapCompose(remove_tags), output_proccessor = TakeFirst())
-    url = scrapy.Field()
-    address = scrapy.Field()
-    rate = scrapy.Field()
-    votes = scrapy.Field()
-    location = scrapy.Field()
-    rest_type = scrapy.Field()
-    cuisines = scrapy.Field()
-    approx_cost = scrapy.Field()
+    address = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    branch = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    cost = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    cuisines = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    featured_in = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    hours = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    location = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    name = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    rate = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    r_type = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    url = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    votes = scrapy.Field(input_proccessor=MapCompose(remove_tags), output_proccessor=TakeFirst())
+    
